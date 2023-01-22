24b0a3bdbd076a99964472825d763fb63c4a9bef
clrminded <the_rocket_scientist@icloud.com>
2023-01-21 16:43:02 -0800

    changed to typescript on App

--- App.tsx
+++ App.tsx
@@ -6,8 +6,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen';
 
-
-
 const Stack = createNativeStackNavigator();
 
 function App() {
